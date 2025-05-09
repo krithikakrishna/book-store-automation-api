60590b91f6b17ae48d85d62d10276680c028d723
krithikakrishna <krithikakrishna889@gmail.com>
2025-05-09 09:51:00 +0530

    Cucumber report added

--- src/test/java/Runner/CucumberRunner.java
+++ src/test/java/Runner/CucumberRunner.java
@@ -2,7 +2,13 @@ package Runner;
 import io.cucumber.testng.CucumberOptions;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 
-@CucumberOptions(features = "src/test/resources/features",glue = {"Steps","Hooks"}, plugin = {"pretty","io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"},tags = "")
+@CucumberOptions(features = "src/test/resources/features",
+                 glue = {"Steps","Hooks"}, 
+                 plugin = {"pretty","io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm",//Allure report
+                            "html:target/cucumber-reports/cucumber.html", // HTML report
+                            "json:target/cucumber-reports/cucumber.json", // JSON report
+                            "junit:target/cucumber-reports/cucumber.xml"},  // JUnit XML report},
+                 tags = "")
 public class CucumberRunner extends AbstractTestNGCucumberTests {
 }
 
